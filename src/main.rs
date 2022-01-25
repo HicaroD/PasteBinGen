be0a9a774c4c403bee7f9b624b5836920d5f62eb
HicaroD <hdanrlley1@gmail.com>
2022-01-25 12:54:39 -0300

    Adding `main` function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod config;
+
 fn main() {
-    println!("Hello, world!");
+    let config_file_path = "config.json".to_string();
+
+    if config::config_file_exists(&config_file_path){
+        println!("Config file exists!");
+    }
+
+    else {
+        let config = config::APIConfiguration {
+            api_key: "something".to_string(),
+        };
+
+        config::create_configuration_file(&config);
+    }
 }
