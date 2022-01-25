ed06460dc9cdd31f540a75429b73d5869f668a8b
HicaroD <hdanrlley1@gmail.com>
2022-01-25 15:03:10 -0300

    Deleting function that checks configuration file
    It is useless right now

--- src/config.rs
+++ src/config.rs
@@ -1,17 +1,12 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 use std::fs::File;
-use std::path::Path;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct APIConfiguration {
     pub api_key: String,
 }
 
-pub fn config_file_exists(path: &str) -> bool {
-    return Path::new(path).exists()
-}
-
-pub fn create_configuration_file(configuration: &APIConfiguration){
+pub fn create_configuration_file(configuration: &APIConfiguration) {
     let config_file = File::create("config.json").expect("Can't create file");
     serde_json::to_writer_pretty(config_file, configuration).expect("Invalid configuration");
 }
