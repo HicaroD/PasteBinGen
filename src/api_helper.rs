5b2603c0768849f8fabaac8703b6c278775b8e88
HicaroD <hdanrlley1@gmail.com>
2022-01-29 00:31:48 -0300

    Creating function to send POST request

--- src/api_helper.rs
+++ src/api_helper.rs
@@ -2,18 +2,22 @@ use std::collections::HashMap;
 
 static PASTE_BIN_URL: &str = "https://pastebin.com/api/api_post.php";
 
-fn get_post_parameters(api_key: &str) -> HashMap<&'static str, &'static str>{
+fn get_post_parameters(api_key: String, file_data: String) -> HashMap<&'static str, String> {
     let params = HashMap::from([
-    ("api_dev_key", api_key),
-    ("api_paste_code", "my text"),
-    ("api_option", "paste"),
-]);
+        ("api_dev_key", api_key),
+        ("api_paste_code", file_data),
+        ("api_option", "paste".to_string()),
+    ]);
+
     params
 }
 
-pub async fn send_paste_request() -> Result<reqwest::Response, reqwest::Error> {
+pub async fn post_pastebin(
+    api_key: String,
+    file_path: String,
+) -> Result<reqwest::Response, reqwest::Error> {
     let client = reqwest::Client::new();
-    let params = get_post_parameters();
+    let params = get_post_parameters(api_key, file_path);
     let res = client.post(PASTE_BIN_URL).form(&params).send().await;
     res
 }
