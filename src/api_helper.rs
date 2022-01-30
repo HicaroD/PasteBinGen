c94c49ad4ea077eda725d61ae81bc2af14c00d5e
HicaroD <hdanrlley1@gmail.com>
2022-01-30 17:25:13 -0300

    Adding `paste_format` to POST parameters

--- src/api_helper.rs
+++ src/api_helper.rs
@@ -2,22 +2,22 @@ use std::collections::HashMap;
 
 static PASTE_BIN_URL: &str = "https://pastebin.com/api/api_post.php";
 
-fn get_post_parameters(api_key: String, file_data: String) -> HashMap<&'static str, String> {
-    let params = HashMap::from([
+fn get_post_parameters(api_key: String, file_data: String, paste_format: String) -> HashMap<&'static str, String> {
+    HashMap::from([
         ("api_dev_key", api_key),
         ("api_paste_code", file_data),
+        ("api_paste_format", paste_format),
         ("api_option", "paste".to_string()),
-    ]);
-
-    params
+    ])
 }
 
 pub async fn post_pastebin(
     api_key: String,
     file_path: String,
+    paste_format: String,
 ) -> Result<reqwest::Response, reqwest::Error> {
     let client = reqwest::Client::new();
-    let params = get_post_parameters(api_key, file_path);
+    let params = get_post_parameters(api_key, file_path, paste_format);
     let res = client.post(PASTE_BIN_URL).form(&params).send().await;
     res
 }
