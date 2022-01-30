32c2c100119bf9e82df0f59654238078d9c6d0a4
HicaroD <hdanrlley1@gmail.com>
2022-01-30 17:46:45 -0300

    Adding `paste_name` to POST parameters

--- src/api_helper.rs
+++ src/api_helper.rs
@@ -6,12 +6,14 @@ fn get_post_parameters(
     api_key: String,
     file_data: String,
     paste_format: String,
+    paste_name: String,
 ) -> HashMap<&'static str, String> {
     HashMap::from([
         ("api_dev_key", api_key),
         ("api_paste_code", file_data),
         ("api_paste_format", paste_format),
-        ("api_option", "paste".to_string()),
+        ("api_paste_name", paste_name),
+        ("api_option", String::from("paste")),
     ])
 }
 
@@ -19,9 +21,10 @@ pub async fn post_pastebin(
     api_key: String,
     file_path: String,
     paste_format: String,
+    paste_name: String,
 ) -> Result<reqwest::Response, reqwest::Error> {
     let client = reqwest::Client::new();
-    let params = get_post_parameters(api_key, file_path, paste_format);
+    let params = get_post_parameters(api_key, file_path, paste_format, paste_name);
     let res = client.post(PASTE_BIN_URL).form(&params).send().await;
     res
 }
