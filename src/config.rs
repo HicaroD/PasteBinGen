c36cc6d41db8a5c87f088d2bb494fd354f96dd48
HicaroD <hdanrlley1@gmail.com>
2022-02-19 20:14:55 -0300

    Implementing method to check if it is API key

--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs;
 use std::fs::File;
-use std::path::PathBuf;
+use std::path::Path;
 
 type JSONData = HashMap<String, String>;
 
@@ -11,22 +11,14 @@ pub struct APIConfiguration {
     pub api_key: String,
 }
 
-pub fn config_file_exists(file_path: &PathBuf) -> bool {
-    file_path.is_file()
-}
-
-pub fn create_configuration_file(file_path: &PathBuf) -> File {
-    File::create(file_path).expect("Unable to create configuration file")
-}
-
-pub fn write_api_key_to_config_file(
-    config_file_path: &PathBuf,
-    configuration: &APIConfiguration,
-) {
-    if !config_file_exists(&config_file_path) {
-        let _ = create_configuration_file(&config_file_path);
+impl APIConfiguration {
+    fn is_default_key(&self) -> bool {
+        self.api_key == "default"
     }
-    if api_key_flag_was_passed(&configuration) {
+}
+
+pub fn write_api_key_to_config_file(config_file_path: &Path, configuration: &APIConfiguration) {
+    if !configuration.is_default_key() {
         let config_file = File::options()
             .write(true)
             .truncate(true)
@@ -36,26 +28,12 @@ pub fn write_api_key_to_config_file(
     }
 }
 
-pub fn api_key_flag_was_passed(configuration: &APIConfiguration) -> bool {
-    !configuration.api_key.eq("default")
-}
-
-pub fn deserialize_configuration_file(config_file_path: &PathBuf) -> Option<JSONData> {
-    if config_file_exists(&config_file_path) {
-        let config_file = fs::read_to_string(config_file_path);
-
-        match config_file {
-            Ok(config_file_content) => {
-                let config_file_data =
-                    serde_json::from_str::<JSONData>(&config_file_content).unwrap();
-                Some(config_file_data)
-            }
-
-            Err(err) => panic!("Error while trying to read configuration file: {}", err),
-        }
-    } else {
-        None
-    }
+pub fn deserialize_configuration_file(
+    config_file_path: &Path,
+) -> Result<JSONData, std::io::Error> {
+    let config_file = fs::read_to_string(config_file_path)?;
+    let config_file_content = serde_json::from_str::<JSONData>(&config_file)?;
+    Ok(config_file_content)
 }
 
 #[cfg(test)]
@@ -65,18 +43,11 @@ mod tests {
 
     #[test]
     fn test_deserializer() {
-        let config_file_path = "src/tests/config_example.json";
-        let expected_result =
-            HashMap::from([("api_key".to_string(), "SOME_RANDOM_API_KEY".to_string())]);
+        let config_file_path = Path::new("src/tests/config_example.json");
+        let expected_result = HashMap::from([("api_key".to_string(), "SOME_RANDOM_API_KEY".to_string())]);
         assert_eq!(
-            deserialize_configuration_file(config_file_path),
-            Some(expected_result)
+            deserialize_configuration_file(config_file_path).unwrap(),
+            expected_result
         )
     }
-
-    #[test]
-    fn test_if_config_file_exists() {
-        assert_eq!(config_file_exists("src/tests/config_example.json"), true);
-    }
-
 }
