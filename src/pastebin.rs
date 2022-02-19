3f31e8b15cc6170cf4e2e4b36b43f04b75634c0d
HicaroD <hdanrlley1@gmail.com>
2022-02-19 18:51:25 -0300

    `pastebin.rs` instead of `api_helper.rs`
    I'm building functionalities of PasteBin, like `post`. That's why the
    new name makes more sense

--- src/pastebin.rs
+++ src/pastebin.rs
@@ -2,26 +2,26 @@ use std::collections::HashMap;
 
 static PASTE_BIN_URL: &str = "https://pastebin.com/api/api_post.php";
 
-fn get_post_parameters(
-    api_key: String,
-    file_data: String,
-    paste_format: String,
-    paste_name: String,
-) -> HashMap<&'static str, String> {
+fn get_post_parameters<'a>(
+    api_key: &'a str,
+    file_data: &'a str,
+    paste_format: &'a str,
+    paste_name: &'a str,
+) -> HashMap<&'static str, &'a str> {
     HashMap::from([
         ("api_dev_key", api_key),
         ("api_paste_code", file_data),
         ("api_paste_format", paste_format),
         ("api_paste_name", paste_name),
-        ("api_option", String::from("paste")),
+        ("api_option", "paste"),
     ])
 }
 
-pub async fn post_pastebin(
-    api_key: String,
-    file_path: String,
-    paste_format: String,
-    paste_name: String,
+pub async fn post(
+    api_key: &str,
+    file_path: &str,
+    paste_format: &str,
+    paste_name: &str,
 ) -> Result<reqwest::Response, reqwest::Error> {
     let client = reqwest::Client::new();
     let params = get_post_parameters(api_key, file_path, paste_format, paste_name);
