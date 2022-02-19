8214582e49b53fce842b03ea79ffd12a015120f5
HicaroD <hdanrlley1@gmail.com>
2022-02-19 20:15:22 -0300

    Adding command line parser and improving error handling

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,75 @@
-mod api_helper;
-mod command_line_parser;
-mod config;
-mod file_handler;
-
+use std::fs;
+use clap::Parser;
 use reqwest::Error;
-use dirs;
+
+mod pastebin;
+mod config;
+
+use config::{APIConfiguration, write_api_key_to_config_file, deserialize_configuration_file};
+
+#[derive(Parser, Debug)]
+#[clap(author = "PasteBinGen", version, about = "A simple CLI for writing PasteBin texts.", long_about = None)]
+pub struct Args {
+    /// PasteBin API key
+    #[clap(short, long, default_value = "default", required = false)]
+    pub api_key: String,
+
+    /// Path to file that you want to upload
+    #[clap(short, long, required = true)]
+    pub path: String,
+
+    /// Syntax highlighting options
+    #[clap(short = 'f', long, required = false, default_value = "text")]
+    pub paste_format: String,
+
+    /// Paste name
+    #[clap(short = 'n', long, required = false, default_value = "untitled")]
+    pub paste_name: String,
+}
+
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let args = command_line_parser::get_command_line_arguments();
+    let args = Args::parse();
 
-    let config = config::APIConfiguration {
+    let config = APIConfiguration {
         api_key: args.api_key,
     };
 
-    let mut config_file_path = dirs::home_dir().unwrap();
+    let mut config_file_path = match dirs::home_dir() {
+        None => {
+            eprintln!("Missing home directory");
+            std::process::exit(1);
+        },
+        Some(path) => path,
+    };
     config_file_path.push("config.json");
 
-    config::write_api_key_to_config_file(&config_file_path, &config);
+    write_api_key_to_config_file(&config_file_path, &config);
 
-    let config_file = config::deserialize_configuration_file(&config_file_path).unwrap();
-    let api_key = config_file.get("api_key").unwrap();
+    let config_file = deserialize_configuration_file(&config_file_path).unwrap(); // ERROR HANDLING
 
-    let file_data = file_handler::get_file_as_string(args.path);
-    let res = api_helper::post_pastebin(
-        api_key.to_string(),
-        file_data,
-        args.paste_format,
-        args.paste_name,
+    let api_key = match config_file.get("api_key") {
+        Some(key) => key,
+        None => {
+            eprintln!("Missing API key");
+            std::process::exit(1);
+        }
+    };
+
+    let file_data = match fs::read_to_string(args.path) {
+        Ok(f) => f,
+        Err(e) => {
+            eprintln!("{e}");
+            std::process::exit(1);
+        }
+    };
+
+    let res = pastebin::post(
+        api_key,
+        &file_data,
+        &args.paste_format,
+        &args.paste_name
     );
     println!("{}", res.await?.text().await?);
     Ok(())
